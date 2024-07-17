3d4d309921a3c72d16d8889a06cebc73bf9b24a8
bmaggiano <brandon.maggiano@gmail.com>
2024-07-17 12:47:05 -0700

    mapping through blog content now

--- components/ui/blog.tsx
+++ components/ui/blog.tsx
@@ -1,12 +1,17 @@
 import { ContentCard } from "./contentCard";
+import { blogContent } from "@/app/blogContent";
 
 export default function Blog() {
     return (
-        <main>
-            <div className="relative flex h-[200px] w-full flex-col items-center justify-center overflow-hidden rounded-lg">
+        <main className="mt-8">
+            <div className="relative flex w-full flex-col items-center justify-center overflow-hidden rounded-lg">
                 <h2 className="text-white text-3xl mb-12 font-medium sm:text-3xl md:text-6xl">Blog</h2>
             </div>
-            <ContentCard />
+            <div className="flex sm:flex-row flex-col justify-center sm:justify-evenly gap-y-2 sm:gap-y-0 gap-x-12">
+                {blogContent.map((blog, index) => (
+                    <ContentCard key={index} {...blog} />
+                ))}
+            </div>
         </main>
 
     )
