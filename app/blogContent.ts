536479830a020b1344d0a846a4b7eba2a77264aa
bmaggiano <brandon.maggiano@gmail.com>
2024-07-17 12:46:33 -0700

    made blog content file to export blogs to and from

---
+++ app/blogContent.ts
@@ -0,0 +1,37 @@
+export const blogContent = [{
+    id: "fantasy-football",
+    title: "Fantasy Football",
+    description: "Dive into how I recreated the Sleeper Fantasy App dashboard, pulling in real-time data from users' teams.",
+    image: "/footballBlog.jpg",
+    readTime: 5,
+    content: `
+            <p>Welcome to my fantasy football project! Here, I've recreated the Sleeper Fantasy App dashboard...</p>
+            <p>Using real-time data from users' teams...</p>
+            <p>Stay tuned for AI enhancements...</p>
+        `,
+},
+{
+    id: "my-journey",
+    title: "My Journey",
+    description: "Follow my journey from Realtor to Software Engineer. Sharing insights from my first year in the industry and lessons learned along the way.",
+    image: "/journey.jpg",
+    readTime: 10,
+    lighter: true,
+    content: `
+            <p>My journey began as a Realtor...</p>
+            <p>Transitioning to Software Engineering...</p>
+            <p>Lessons learned in my first year...</p>
+        `,
+},
+{
+    id: "captions-made-easy",
+    title: "Captions Made Easy",
+    description: "Build an AI based instagram caption generator with me. This tool makes creating captivating captions a breeze!",
+    image: "/ai.jpg",
+    readTime: 5,
+    content: `
+            <p>Creating captions has never been easier...</p>
+            <p>With the help of my buddy Sid...</p>
+            <p>Using AI to generate amazing captions...</p>
+        `,
+}]
