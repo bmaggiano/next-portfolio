fe4760d168940b95c0a05d6f1c0ec69d1144b8f9
bmaggiano <brandon.maggiano@gmail.com>
2024-07-16 21:43:49 -0700

    added jumbotron

---
+++ components/ui/jumbotron.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./button";
+
+export default function Jumbotron() {
+    return (
+        <main className="flex flex-col items-center justify-center gap-y-4 px-4 py-16 sm:px-6 lg:px-8">
+            <h1 className="text-white text-center font-semibold text-3xl sm:text-5xl md:text-6xl lg:text-7xl">From Concept to Code</h1>
+            <p className="max-w-[42rem] text-center leading-normal text-muted-foreground sm:text-xl sm:leading-8">Crafting React and Next.js magic. Step into my portfolio, where code meets creativity.</p>
+            <Button variant="outline">
+                Follow along on X
+            </Button>
+        </main>
+    )
+}
