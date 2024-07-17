c004d6ad6629afe6896df9edbcc9baebf49ad307
bmaggiano <brandon.maggiano@gmail.com>
2024-07-17 10:15:46 -0700

    added aceternity content card

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -59,6 +59,10 @@ const config = {
         sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
+        move: {
+          "0%": { transform: "translateX(-200px)" },
+          "100%": { transform: "translateX(200px)" },
+        },
         "accordion-down": {
           from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
@@ -77,6 +81,7 @@ const config = {
         },
       },
       animation: {
+        move: "move 5s linear infinite",
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         marquee: "marquee var(--duration) linear infinite",
