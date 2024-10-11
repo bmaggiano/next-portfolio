2995fd2e422f6452bca8bf3efb7f27d03b8a7783
bmaggiano <brandon.maggiano@gmail.com>
2024-10-11 09:23:17 -0700

    changes to projects section making it more readily available to folks who want to find a specific project

--- app/blog/[blogId]/page.tsx
+++ app/blog/[blogId]/page.tsx
@@ -1,49 +1,74 @@
 "use client";
-import { useParams } from 'next/navigation';
-import { blogContent } from '../../../app/blogContent';
-import Image from 'next/image';
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList } from '@/components/ui/breadcrumb';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-
-
+import { useParams } from "next/navigation";
+import { blogContent } from "../../../app/blogContent";
+import Image from "next/image";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+} from "@/components/ui/breadcrumb";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default function BlogPost() {
-    const params = useParams();
-    const blog = blogContent.find(blog => blog.id === params?.blogId);
+  const params = useParams();
+  const blog = blogContent.find((blog) => blog.id === params?.blogId);
 
-    if (!blog) {
-        return <p className='text-white'>Blog post not found.</p>;
-    }
+  if (!blog) {
+    return <p className="text-white">Blog post not found.</p>;
+  }
 
-    return (
-        <main className="max-w-3xl mx-auto px-4 mt-8 text-white">
-            <div className=" flex w-full flex-col justify-center overflow-hidden rounded-lg">
-                <Breadcrumb className='mb-0 sm:mb-4'>
-                    <BreadcrumbList>
-                        <BreadcrumbItem>
-                            <BreadcrumbLink href="/"> &lt; Back</BreadcrumbLink>
-                        </BreadcrumbItem>
-                    </BreadcrumbList>
-                </Breadcrumb>
-                <div className='flex flex-col space-y-4'>
-                    <p><span className='text-gray-400'>Published on {blog.published}</span></p>
-                    <h2 className="text-white text-3xl mb-4 font-medium sm:text-3xl md:text-4xl">{blog.title}</h2>
-                    <div className='flex flex-row items-center space-x-4'>
-                        <Avatar>
-                            <AvatarImage src="/bmaggiano.jpeg" alt="bmaggiano avatar" />
-                            <AvatarFallback>B</AvatarFallback>
-                        </Avatar>
-                        <div>
-                            <p className='text-sm text-gray-400'>Brandon Maggiano</p>
-                            <a href="https://twitter.com/brandonmaggiano" target="_blank" rel="noreferrer" className='text-xs text-gray-400 hover:underline'>@bmaggiano</a>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <main className="max-w-3xl mx-auto px-4 mt-8 text-white">
+      <div className=" flex w-full flex-col justify-center overflow-hidden rounded-lg">
+        <Breadcrumb className="mb-0 sm:mb-4">
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/"> &lt; Back</BreadcrumbLink>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+        <div className="flex flex-col space-y-4">
+          <p>
+            <span className="text-gray-400">Published on {blog.published}</span>
+          </p>
+          <h2 className="text-white text-3xl mb-4 font-medium sm:text-3xl md:text-4xl">
+            {blog.title}
+          </h2>
+          <div className="flex flex-row items-center space-x-4">
+            <Avatar>
+              <AvatarImage src="/bmaggiano.jpeg" alt="bmaggiano avatar" />
+              <AvatarFallback>B</AvatarFallback>
+            </Avatar>
+            <div>
+              <p className="text-sm text-gray-400">Brandon Maggiano</p>
+              <a
+                href="https://twitter.com/brandonmaggiano"
+                target="_blank"
+                rel="noreferrer"
+                className="text-xs text-gray-400 hover:underline"
+              >
+                @bmaggiano
+              </a>
             </div>
-            <div className="flex flex-col items-center">
-                <Image layout='intrinsic' height={50} width={800} src={blog.blogImage} alt={blog.title} className="max-h-[200px] sm:max-h-[300px] object-cover object-bottom rounded-lg my-8" />
-                <article className="text-gray-400 mb-8" dangerouslySetInnerHTML={{ __html: blog.content }} />
-            </div>
-        </main>
-    );
-};
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col items-center">
+        <Image
+          layout="intrinsic"
+          priority
+          height={50}
+          width={800}
+          src={blog.blogImage}
+          alt={blog.title}
+          className="max-h-[200px] sm:max-h-[300px] object-cover object-bottom rounded-lg my-8"
+        />
+        <article
+          className="text-gray-400 mb-8"
+          dangerouslySetInnerHTML={{ __html: blog.content }}
+        />
+      </div>
+    </main>
+  );
+}
--- components/ui/projects.tsx
+++ components/ui/projects.tsx
@@ -1,102 +1,107 @@
 "use client";
 
-import { cn } from "@/lib/utils";
-import Marquee from "@/components/magicui/marquee";
+import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { Card, CardContent } from "./card";
+import { motion } from "framer-motion";
 
 const projects = [
-    {
-        name: "Stuart AI",
-        body: "Your AI friendly fantasy football knowledge base",
-        img: "/football.png",
-        url: "https://sleeper-dashboard.vercel.app/"
-    },
-    {
-        name: "Instagram Caption Generator",
-        body: "AI generated captions for Instagram posts with OpenAI",
-        img: "/igcap.png",
-        url: "https://instagram-caption-generator-pi.vercel.app/"
-    },
-    {
-        name: "Healthcare Download",
-        body: "Over a year of work experience helping create the Mint.com of healthcare",
-        img: "/hcdl.png",
-        url: "https://www.healthcare-download.com/"
-    },
-    {
-        name: "The Casual's Caddie",
-        body: "Track your golf club distances and see how you compare to pros",
-        img: "/golf.png",
-        url: "https://the-casuals-caddie.herokuapp.com/"
-    },
-    {
-        name: "Note Taking App",
-        body: "Basic CRUD app for taking notes",
-        img: "/notetake.png",
-        url: "https://study-sheet.vercel.app/"
-    },
-    {
-        name: "Three Tabs Three Ways",
-        body: "Fun, electric, UI showcasing different ways to use tabs",
-        img: "/tabs.png",
-        url: "https://three-tabs-three-ways.vercel.app/"
-    },
+  {
+    name: "Stuart AI",
+    body: "Your AI friendly fantasy football knowledge base",
+    img: "/football.png",
+    url: "https://sleeper-dashboard.vercel.app/",
+  },
+  {
+    name: "Healthcare Download",
+    body: "Over a year of work experience helping create the Mint.com of healthcare",
+    img: "/hcdl.png",
+    url: "https://www.healthcare-download.com/",
+  },
+  {
+    name: "Instagram Caption Generator",
+    body: "AI generated captions for Instagram posts with OpenAI",
+    img: "/igcap.png",
+    url: "https://instagram-caption-generator-pi.vercel.app/",
+  },
+  {
+    name: "The Casual's Caddie",
+    body: "Track your golf club distances and see how you compare to pros",
+    img: "/golf.png",
+    url: "https://the-casuals-caddie.herokuapp.com/",
+  },
+  {
+    name: "Note Taking App",
+    body: "Basic CRUD app for taking notes",
+    img: "/notetake.png",
+    url: "https://study-sheet.vercel.app/",
+  },
+  {
+    name: "Three Tabs Three Ways",
+    body: "Fun, electric, UI showcasing different ways to use tabs",
+    img: "/tabs.png",
+    url: "https://three-tabs-three-ways.vercel.app/",
+  },
 ];
 
-const firstRow = projects.slice(0, projects.length / 2);
-const secondRow = projects.slice(projects.length / 2);
-
 const ReviewCard = ({
-    img,
-    name,
-    body,
-    url,
+  img,
+  name,
+  body,
+  url,
 }: {
-    img: string;
-    name: string;
-    body: string;
-    url: string;
+  img: string;
+  name: string;
+  body: string;
+  url: string;
 }) => {
-    return (
-        <Link href={url} target="_blank" prefetch={true} className="block">
-            <figure
-                className={cn(
-                    "relative w-64 h-36 cursor-pointer overflow-hidden rounded-xl border p-4",
-                    "border-gray-50/[.1] bg-gray-50/[.10] hover:bg-gray-50/[.15]",
-                )}
-            >
-                <div className="flex flex-row items-center gap-2">
-                    <Image className="rounded-full bg-white" width={32} height={32} alt={name} src={img} />
-                    <div className="flex flex-col">
-                        <figcaption className="text-sm font-medium text-white">
-                            {name}
-                        </figcaption>
-                    </div>
-                </div>
-                <blockquote className="text-gray-400 mt-2 text-sm">{body}</blockquote>
-            </figure>
-        </Link>
-    );
+  return (
+    <Link href={url} prefetch={true}>
+      <Card className="my-4 bg-gray-50/[.10] border-none shadow-lg">
+        <CardContent className="p-4 flex items-center space-x-4">
+          <div className="bg-white p-2 rounded-full">
+            <Image
+              className="bg-white"
+              width={32}
+              height={32}
+              alt={name}
+              src={img}
+            />
+          </div>
+          <div>
+            <h2 className="text-white font-semibold">{name}</h2>
+            <p className="text-gray-400 text-sm">{body}</p>
+          </div>
+        </CardContent>
+      </Card>
+    </Link>
+  );
 };
 
 export function Projects() {
-    return (
-        <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg">
-            <h2 className="text-white text-3xl mb-12 font-medium sm:text-3xl md:text-6xl">Projects</h2>
-            <Marquee pauseOnHover className="[--duration:40s]">
-                {firstRow.map((review) => (
-                    <ReviewCard key={review.name} {...review} />
-                ))}
-            </Marquee>
-            <Marquee reverse pauseOnHover className="[--duration:40s]">
-                {secondRow.map((review) => (
-                    <ReviewCard key={review.name} {...review} />
-                ))}
-            </Marquee>
-            {/* Overlay gradients for aesthetic effect */}
-            <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-black"></div>
-            <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-black"></div>
-        </div>
-    );
+  const [hoveredProject, setHoveredProject] = useState<string | null>(null);
+
+  return (
+    <div className="flex w-full flex-col items-center justify-center overflow-hidden">
+      <h2 className="text-white text-3xl mb-12 font-medium sm:text-3xl md:text-6xl">
+        Projects
+      </h2>
+      <div className="w-full sm:w-4/5">
+        {projects.map((project) => {
+          return (
+            <motion.div
+              key={project.name}
+              className="rounded-md cursor-pointer hover:bg-gray-50/[.10]"
+              whileHover={{ scale: 1.03 }}
+              onHoverStart={() => setHoveredProject(project.name)}
+              onHoverEnd={() => setHoveredProject(null)}
+            >
+              <ReviewCard key={project.name} {...project} />
+            </motion.div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
